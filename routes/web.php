3b3b56e5b6340f50b673b54d6f9e7cc351f3ef03
Mateus Tavares <mateus460@gmail.com>
2018-02-03 15:11:34 -0300

    add routes

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,93 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::prefix('account')->group(function () {
+	Route::get('/register', function () {
+	    return view('home');
+	});
+
+	Route::post('/register', function () {
+	    return view('home');
+	});
+
+	Route::get('/recovery', function () {
+	    return view('home');
+	});
+
+	Route::post('/recovery', function () {
+	    return view('home');
+	});
+
+	Route::get('/login', function () {
+	    return view('home');
+	});
+
+	Route::post('/login', function () {
+	    return view('home');
+	});
+
+	Route::get('/logout', function () {
+	    return view('home');
+	});
+
+	Route::get('/edit', function () {
+	    return view('home');
+	});
+
+	Route::put('/update', function () {
+	    return view('home');
+	});
+
+	Route::prefix('topics')->group(function () {
+		Route::get('/', function () {
+		    return view('home');
+		});
+
+		Route::get('/create', function () {
+		    return view('home');
+		});
+
+		Route::post('/publish', function () {
+		    return view('home');
+		});
+
+		Route::delete('/{id}/delete', function ($id) {
+		    return view('home');
+		});
+
+		Route::get('/{id}/edit', function ($id) {
+		    return view('home');
+		});
+
+		Route::put('/{id}/update', function ($id) {
+		    return view('home');
+		});
+
+		Route::get('/{id}/reply', function ($id) {
+		    return view('home');
+		});
+
+		Route::post('/{id}/reply/publish', function ($id) {
+		    return view('home');
+		});
+	});
+});
+
+Route::prefix('user')->group(function () {
+	Route::get('/{id}/profile', function ($id) {
+	    return "Perfil do usuário {$id}.";
+	});
+});
+
+Route::prefix('topics')->group(function () {
+	Route::get('/', function () {
+	    return view('home');
+	});
+
+	Route::get('/{id}/{slug}', function ($id, $slug) {
+	    return view('home');
+	});
 });
